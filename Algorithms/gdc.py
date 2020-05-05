6913425ed2a2f9628e179df5f5d0cf11791c7aa7
asolanky <alwinsolanky@gmail.com>
2020-05-05 20:01:53 +0100

    gcd.py

---
+++ Algorithms/gdc.py
@@ -0,0 +1,12 @@
+# Find the greatest common denominator of two numbers
+# using Euclid's algorithm
+def gcd(a, b):
+	while(b != 0):
+		t = a
+		a = b
+		b = t % b
+
+	return a
+
+print(gcd(60, 96)) # is 12
+print(gcd(20, 8)) # is 4
